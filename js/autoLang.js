856ac014411ddbceec58c4b350422cb6cae672e4
alataq <alataqdev@gmail.com>
2025-01-30 13:05:18 -0500

    Fixing javascript

--- js/autoLang.js
+++ js/autoLang.js
@@ -1,7 +1,15 @@
 const language = localStorage.getItem("language");
 const splittedPath = window.location.pathname.split("/");
+const hostname = window.location.hostname
+const port = window.location.port;
+const protocol = window.location.protocol;
 
 function execute(){
+    if(hostname !== "nerexon.com"){
+        window.location.href = "https://nerexon.com/";
+        return;
+    }
+
     if(!splittedPath)return;
     if(!language)return;
 
@@ -10,9 +18,6 @@ function execute(){
     if(pathLanguage){
         if(pathLanguage === language)return;
 
-        const port = window.location.port;
-        const protocol = window.location.protocol;
-
         splittedPath[0] = window.location.hostname + (port ? ":" + port : "");
         splittedPath[2] = language;
         
@@ -20,15 +25,6 @@ function execute(){
         window.location.href = fullPath;
         
     } else {
-        const hostname = window.location.hostname
-        const port = window.location.port;
-        const protocol = window.location.protocol;
-
-        if(hostname !== "nerexon.com"){
-            window.location.href = "https://nerexon.com/";
-            return;
-        }
-
         splittedPath[0] = hostname + (port ? ":" + port : "");
         splittedPath[1] = "page";
         splittedPath[2] = language;
