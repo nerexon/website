b30fd59abc886d3833bde6256daf9bd2a762559f
Alataq <nerexonadmin@proton.me>
2025-02-01 12:43:23 -0500

    Fixing infinite redirection

--- js/autoLang.js
+++ js/autoLang.js
@@ -20,7 +20,7 @@ function execute(){
         window.location.href = fullPath;
         
     } else {
-        if(hostname !== "nerexon.com"){
+        if(hostname !== "nerexon.com"  && !language){
             window.location.href = "https://nerexon.com/";
             return;
         }
