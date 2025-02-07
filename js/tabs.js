6890b996fa3f5822f7281304766a81c1db3376c1
Alataq <nerexonadmin@proton.me>
2025-02-07 10:26:36 -0500

    Addign jsdos support

--- js/tabs.js
+++ js/tabs.js
@@ -1,6 +1,15 @@
+/**
+ * @const {HTMLCollectionOf<Element>} tabulationMenus - A collection of all elements with the class "tab-nav" that contain tab navigation.
+ * @const {HTMLElement} mainSecret - The main secret content element with ID "secret-0".
+ */
 const tabulationMenus = document.getElementsByClassName("tab-nav");
 const mainSecret = document.getElementById("secret-0");
 
+/**
+ * Shows the specified element with a transition effect.
+ * 
+ * @param {HTMLElement} element - The element to be shown.
+ */
 function show(element) {
     element.style.display = "block";
     element.style.opacity = 0;
@@ -12,6 +21,12 @@ function show(element) {
     }, 10);
 }
 
+/**
+ * Hides the specified element with a transition effect and executes a callback once completed.
+ * 
+ * @param {HTMLElement} element - The element to be hidden.
+ * @param {function} [callback] - The callback function to execute once the hiding transition is complete.
+ */
 function hide(element, callback) {
     element.style.opacity = 0;
     element.style.transform = "scale(0.9)";
@@ -21,6 +36,10 @@ function hide(element, callback) {
     }, 300);
 }
 
+/**
+ * Initializes the tabulation menu interactions and binds event listeners to the buttons.
+ * It shows and hides content sections based on user interaction with the tab buttons.
+ */
 function execute() {
     if (!tabulationMenus) return;
     if (!mainSecret) return;
@@ -56,4 +75,5 @@ function execute() {
     }
 }
 
+// Initialize the tabulation menu after the DOM content is loaded.
 document.addEventListener("DOMContentLoaded", execute);
